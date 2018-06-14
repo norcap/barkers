26fb85e7e82cb3c6b98d898ecac8df6daf9b4670
Philipp T <ptrentmann@norcap.com>
2018-06-14 10:44:46 +0200

    in progress commit
    just to make sure the next steps won't destroy the project...

--- src/index.js
+++ src/index.js
@@ -2,46 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import firebase from 'firebase'
 import reducer from './Reducers/reducer'
 import initialState from './Reducers/initialState'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-//const firebase = require('../node_modules/firebase')
+import { receiveChatMessage } from './service'
 
-const config = {
-  apiKey: 'AIzaSyCu2SjX2X8tZgwXyYEdQOjy8VKGCFQM5N8',
-  authDomain: 'mag-messenger.firebaseapp.com',
-  databaseURL: 'https://mag-messenger.firebaseio.com',
-  projectId: 'mag-messenger',
-  storageBucket: '',
-  messagingSenderId: '303315501474'
-}
-
-firebase.initializeApp(config)
-
-const database = firebase.database()
-
-function createNewUser(userId, name, email, password) {
-  database.ref('users/' + userId).set({
-    username: name,
-    email: email,
-    password: password
-  })
-}
-
-createNewUser(45, 'John', 'Email', 'Password')
-
-let users = database
-  .ref('users/45')
-  .once('value')
-  .then(user => {
-    user.forEach(item => {
-      console.log(item.val())
-    })
-  })
+receiveChatMessage(7233456)
 
 const store = createStore(
   reducer,
